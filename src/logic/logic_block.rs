9122a65baafa5a058380adde92e710ae4ca14bcb
Evan Pelletier <robotevan24@gmail.com>
2023-12-28 22:00:12 -0500

    fixing cache, adding more test cases

--- src/logic/logic_block.rs
+++ src/logic/logic_block.rs
@@ -2,7 +2,7 @@ use slotmap::{HopSlotMap, new_key_type};
 use std::collections::HashMap;
 use crate::logic::logic_defs::LogicState;
 
-pub const MAX_CACHE_INPUTS: u32 = 32;
+pub const MAX_CACHE_INPUTS: usize = 32;
 
 new_key_type! {
     pub struct LogicBlockPortKey;
@@ -11,7 +11,7 @@ new_key_type! {
 pub struct LogicBlockPort {
     pub state: LogicState,
     pub is_inverted: bool,
-    input_num: u32,
+    input_num: usize,
 }
 
 impl LogicBlockPort {
@@ -34,45 +34,40 @@ impl LogicBlockPort {
 
 pub struct LogicBlock {
     pub inputs: HopSlotMap<LogicBlockPortKey, LogicBlockPort>, 
-    pub num_inputs: u32,
     output: LogicBlockPort,
     input_cache: u32,
     output_cache: HashMap<u32, LogicState>,
+    cache_valid: bool,
 }
 
 impl LogicBlock {
     pub fn new() -> LogicBlock {
         LogicBlock {
             inputs : HopSlotMap::with_key(),
-            num_inputs: 0,
             output: LogicBlockPort::new(),
             input_cache: 0,
             output_cache: HashMap::new(),
+            cache_valid: false,
         }
     }
 
     pub fn add_input(&mut self) -> LogicBlockPortKey {
         let key = self.inputs.insert(LogicBlockPort::new());
         // reset cache
-        self.init_cache();
+        self.update_cache();
         key
     }
 
     pub fn remove_input(&mut self, key: LogicBlockPortKey) {
-        assert!(!self.inputs.is_empty());
         self.inputs.remove(key);
-        self.num_inputs -= 1;
-        // reset cache
-        self.output_cache = HashMap::new();
-        self.input_cache = 0;
-        self.init_cache()
+        self.update_cache();
     }
 
     pub fn set_input(&mut self, input_key: LogicBlockPortKey, state: LogicState) {
         // update the input cache
         let input = self.inputs.get_mut(input_key).unwrap();
         // set cached value
-        if input.input_num < MAX_CACHE_INPUTS {
+        if self.cache_valid {
             if state == LogicState::HIGH {
                 self.input_cache |= 1 << input.input_num;
             } else {
@@ -80,35 +75,48 @@ impl LogicBlock {
             }
             
         }
-        // set actual value
         input.set_state(state);
     }
 
     pub fn set_output(&mut self, state: LogicState) {
-        // if not in cache and under max num cachable inputs, add output to cache
-        if !self.output_cache.contains_key(&self.input_cache){
-            self.output_cache.insert(self.input_cache, state);
+        match(self.output_cache.get(&self.input_cache)) {
+            // not found in cache, need to add entry
+            None => {
+                self.output_cache.insert(self.input_cache, state);
+            }
+            // found in cache, check if value needs to be updated
+            Some(&cached_state) => {
+                if cached_state != state {
+                    self.output_cache.insert(self.input_cache, state);
+                }
+            }
         }
+        // set the output
         self.output.set_state(state)
     }
 
-    fn init_cache(&mut self) {
+    fn update_cache(&mut self) {
+        // invalidate previous cache
+        self.output_cache = HashMap::new();
+        self.input_cache = 0;
+        
         // too many inputs to cache, invalidate cache
-        if self.num_inputs > 32 {
-            self.output_cache = HashMap::new();
-            self.input_cache = 0;
-            return
+        if self.inputs.len() > MAX_CACHE_INPUTS {
+            self.cache_valid = false;
+            return;
         }
         
+        self.cache_valid = true;
         // assign input number to inputs
+        let mut input_num = 0;
         for (_, input) in self.inputs.iter_mut() {
-            input.input_num = self.num_inputs;
-            self.num_inputs += 1;
+            input.input_num = input_num;
+            input_num += 1;
         }
     }
 
     pub fn check_output_cache(&mut self) -> LogicState {
-        if self.num_inputs > MAX_CACHE_INPUTS {
+        if !self.cache_valid {
             return LogicState::INVALID;
         } 
 
@@ -122,8 +130,6 @@ impl LogicBlock {
             }
         }
     }
-
-    
 }
 
 
@@ -136,16 +142,16 @@ mod tests {
     fn test_add_input() {
         let mut logic_block = LogicBlock::new();
         logic_block.add_input();
-        assert_eq!(logic_block.num_inputs, 1);
+        assert_eq!(logic_block.inputs.len(), 1);
     }
 
     #[test]
     fn test_add_and_remove_input() {
         let mut logic_block = LogicBlock::new();
         let in_1 = logic_block.add_input();
-        assert_eq!(logic_block.num_inputs, 1);
+        assert_eq!(logic_block.inputs.len(), 1);
         logic_block.remove_input(in_1);
-        assert_eq!(logic_block.num_inputs, 0);
+        assert_eq!(logic_block.inputs.len(), 0);
     }
 
     #[test]
@@ -156,7 +162,7 @@ mod tests {
         
         // check state is correct, 1 input
         assert_eq!(logic_block.inputs[in_1].state, LogicState::HIGH);
-        assert_eq!(logic_block.num_inputs, 1);
+        assert_eq!(logic_block.inputs.len(), 1);
         // add entry to cache, check if cache entry present
         logic_block.set_output(LogicState::HIGH);
         assert_eq!(logic_block.input_cache, logic_block.input_cache);
@@ -165,8 +171,9 @@ mod tests {
 
     #[test]
     fn test_max_cache() {
+        let mut expected_cache_mask: u32 = 0;
         let mut logic_block = LogicBlock::new();
-        let mut inputs: [LogicBlockPortKey; 32] = unsafe {MaybeUninit::uninit().assume_init()};
+        let mut inputs: [LogicBlockPortKey; MAX_CACHE_INPUTS] = unsafe {MaybeUninit::uninit().assume_init()};
         // add inputs
         for i in 0..inputs.len() {
             inputs[i] = logic_block.add_input();
@@ -175,16 +182,22 @@ mod tests {
         for i in 0..inputs.len() {
             logic_block.set_input(inputs[i], LogicState::HIGH);
             logic_block.set_output(LogicState::HIGH);
+            // verify input cache mask is correct
+            expected_cache_mask |= 1 << i;
+            assert_eq!(logic_block.input_cache, expected_cache_mask);
+            assert_eq!(logic_block.output_cache.len(), i + 1);
+            assert_eq!(logic_block.cache_valid, true);
         }
-        assert_eq!(logic_block.output_cache.len(), 32);
-        assert_eq!(logic_block.num_inputs, 32);
+        assert_eq!(logic_block.output_cache.len(), MAX_CACHE_INPUTS);
+        assert_eq!(logic_block.inputs.len(), MAX_CACHE_INPUTS);
     }
 
     #[test]
     fn test_above_max_cache() {
+        let mut expected_cache_mask: u32 = 0;
         let mut logic_block = LogicBlock::new();
-        let mut inputs: [LogicBlockPortKey; 33] = unsafe {MaybeUninit::uninit().assume_init()};
-        // add inputs
+        let mut inputs: [LogicBlockPortKey; MAX_CACHE_INPUTS] = unsafe {MaybeUninit::uninit().assume_init()};
+        // add 32 inputs
         for i in 0..inputs.len() {
             inputs[i] = logic_block.add_input();
         }
@@ -192,10 +205,46 @@ mod tests {
         for i in 0..inputs.len() {
             logic_block.set_input(inputs[i], LogicState::HIGH);
             logic_block.set_output(LogicState::HIGH);
+            // verify input cache mask is correct
+            expected_cache_mask |= 1 << i;
+            assert_eq!(logic_block.input_cache, expected_cache_mask);
+            assert_eq!(logic_block.output_cache.len(), i + 1);
+            assert_eq!(logic_block.cache_valid, true);
         }
+        // add one more input
+        logic_block.add_input();
         // above max cachable inputs, check if invalidated
-        assert_eq!(logic_block.num_inputs, 33);
+        assert_eq!(logic_block.inputs.len(), 33);
         assert_eq!(logic_block.output_cache.len(), 0);
+        assert_eq!(logic_block.cache_valid, false);
     }
 
+    #[test]
+    fn test_remove_one_after_max() {
+        let mut logic_block = LogicBlock::new();
+        let mut inputs: [LogicBlockPortKey; MAX_CACHE_INPUTS+1] = unsafe {MaybeUninit::uninit().assume_init()};
+        // add 33 inputs
+        for i in 0..inputs.len() {
+            inputs[i] = logic_block.add_input();
+        }
+        // make sure cache is invalid
+        assert_eq!(logic_block.inputs.len(), 33);
+        assert_eq!(logic_block.cache_valid, false);
+        // remove one input and check if valid again
+        logic_block.remove_input(inputs[0]);
+        assert_eq!(logic_block.inputs.len(), 32);
+        assert_eq!(logic_block.cache_valid, true); 
+    }
+
+    #[test]
+    fn test_remove_with_no_inputs() {
+        let mut logic_block = LogicBlock::new();
+        let in_1 = logic_block.add_input();
+        assert_eq!(logic_block.inputs.len(), 1);
+        logic_block.remove_input(in_1);
+        // try to remove a second time
+        assert!(logic_block.inputs.is_empty());
+        logic_block.remove_input(in_1);
+        assert!(logic_block.inputs.is_empty());
+    }
 }
